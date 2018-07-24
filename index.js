b5ba0bdeeaad877417c91265ed13862c6a78a915
mateuszdziuba <mkdziuba@gmail.com>
2018-07-24 23:10:57 +0200

    updated schemas and resolvers

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import {
+  ApolloServer,
+} from 'apollo-server-express';
 import path from 'path';
-import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
+import {
+  fileLoader,
+  mergeTypes,
+  mergeResolvers,
+} from 'merge-graphql-schemas';
+import cors from 'cors';
 
 import models from './models';
 
 const typeDefs = mergeTypes(fileLoader(path.join(__dirname, './schema')));
 const resolvers = mergeResolvers(fileLoader(path.join(__dirname, './resolvers')));
 
-const PORT = 4000;
+const PORT = 8080;
 
 const app = express();
+
+app.use(cors('*'));
 
 const server = new ApolloServer({
   typeDefs,
@@ -22,8 +31,14 @@ const server = new ApolloServer({
     },
   },
 });
-server.applyMiddleware({ app });
+server.applyMiddleware({
+  app,
+});
 
-models.sequelize.sync().then(() => {
-  app.listen({ port: PORT }, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
+models.sequelize.sync({
+  force: false,
+}).then(() => {
+  app.listen({
+    port: PORT,
+  }, () => console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`));
 });
